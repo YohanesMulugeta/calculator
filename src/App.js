f12e258b4b1277b90486d07b0230f89da3d3db35
Yohanes <yohanesmulugeta21@gmail.com>
2023-01-31 15:11:50 +0300

    input with label

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+// import Items from "./Items";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="app">
+      <div className="order_container flex  ">
+        <label className=" label" htmlFor="order">
+          Order Size
+        </label>
+        <input
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          id="order"
+          type="text"
+          className="order_input"
+        />
+      </div>
     </div>
   );
 }
